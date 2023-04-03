62e88954854c1f436d77d1e22ebf98d87563a718
Jade <hendricksjade@gmail.com>
2023-04-03 19:49:04 +0200

    add(oop): adding in the OOP project and creating our first class.

---
+++ OOP/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Human
+    {
+        //members variable
+        public string firstName = "Michael";
+        private string lastName;
+
+        //member method
+        public void IntroduceMySelf()
+        {
+            Console.WriteLine("Hi, I'm {0}", firstName);
+        } 
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //an instance of Human
+            //aka creating an object from a classes blueprint
+            Human denis = new Human();
+
+            //access public variable from outside
+            denis.firstName = "Denis";
+            //call methods on the instance
+            denis.IntroduceMySelf();
+
+            Human micheal = new Human();
+            micheal.IntroduceMySelf();
+
+            Console.Read();
+        }
+    }
+}
