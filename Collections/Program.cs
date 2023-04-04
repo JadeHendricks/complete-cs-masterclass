79272805a94424031359d54c8f648de604bc19db
Jade <hendricksjade@gmail.com>
2023-04-04 14:21:17 +0200

    add(foreach): taking a look into the difference between a for and a foreach method.

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -32,9 +32,46 @@ namespace Collections
             Console.WriteLine("The length of the gradesOfMathStudentsA array is {0}", gradesOfMathStudentsA.Length);
 
 
-
+            //calling methods
+            ForArraysExample();
+            ForEachArraysExample();
+            Challenge();
 
             Console.Read();
+        }
+
+        public static void ForArraysExample()
+        {
+            int[] nums = new int[10];
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = i;
+                Console.WriteLine("FOR LOOP - Element{0} = {1}", i, nums[i]);
+            }
+        }
+
+        public static void ForEachArraysExample()
+        {
+            int[] nums = { 1,2,3,4,5,6,7,8,9,10 };
+            int counter = 0;
+
+            foreach(int i in nums)
+            {
+                Console.WriteLine("FOREACH LOOP - Element{0} = {1}", counter, i);
+                counter++;
+            }
+        }
+
+        //course challenge
+        public static void Challenge()
+        {
+            string[] fiveFriends = { "James", "Johnny", "Stacy", "Lucy", "Eric" };
+
+            foreach(string friend in fiveFriends)
+            {
+                Console.WriteLine("Hey {0} how are you?", friend);
+            }
         }
     }
 }
