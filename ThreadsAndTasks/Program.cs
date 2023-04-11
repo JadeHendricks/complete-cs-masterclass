e71877c94cc05ae48132afaa1b9021a1df77e1bb
Jade <hendricksjade@gmail.com>
2023-04-11 19:03:59 +0200

    add(threads): Taking a look into isAlive and joining on threads.

--- ThreadsAndTasks/Program.cs
+++ ThreadsAndTasks/Program.cs
@@ -11,7 +11,8 @@ namespace ThreadsAndTasks
         {
             //ThreadBasics();
             //ThreadStartAndEnd();
-            ThreadPoolsAndBackgroundThreads();
+            //ThreadPoolsAndBackgroundThreads();
+            JoinAndIsAlive();
             Console.Read();
         }
 
@@ -76,6 +77,9 @@ namespace ThreadsAndTasks
         {
             Enumerable.Range(0, 1000).ToList().ForEach(f =>
             {
+                //ThreadPool are used to run specific work items
+                //The will wait until the next thread is done/has memory before starting another one
+                //if there is memory the threadpool will use it or it won't if not needed
                 ThreadPool.QueueUserWorkItem((o) =>
                 {
                     Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
@@ -83,6 +87,53 @@ namespace ThreadsAndTasks
                     Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
                 });
             });
+        }
+    
+        public static void JoinAndIsAlive()
+        {
+            Console.WriteLine("Main Thread started");
+            Thread thread1 = new Thread(Thread1Function);
+            Thread thread2 = new Thread(Thread2Function);
+
+            //start the threads
+            thread1.Start();
+            thread2.Start();
+
+            //joining threads
+            //joining thread 1 && 2 with the  main thread - blocks the main thread for that time
+            if (thread1.Join(1000))
+            {
+                Console.WriteLine("Thread1Function done");
+            }
+            else
+            {
+                Console.WriteLine("Thread1Function was't done in 1 second");
+            }
+            thread2.Join();
+            Console.WriteLine("Thread2Function done");
+
+            if (thread1.IsAlive)
+            {
+                Console.WriteLine("Thread 1 is still doing stuff");
+            }
+            else
+            {
+                Console.WriteLine("Thread 1 is done");
+            }
+
+            Console.WriteLine("Main Thread ended");
+        }
+
+        //to be used by JoinAndIsAlive()
+        public static void Thread1Function()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread1Function started");
+        }
+
+        public static void Thread2Function()
+        {
+            Console.WriteLine("Thread2Function started");
         }
     }
 }
